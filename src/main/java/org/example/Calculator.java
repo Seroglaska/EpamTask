05d2cc153cb774180b4c1b8dce4986f6fef5a394
Danila <dan.serookiy@mail.ru>
2023-01-22 14:05:07 +0300

    Change files

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -4,7 +4,7 @@ package org.example;
 public class Calculator {
     public void calc(String s) {
         Logic logic = new Logic();
-        by.epam.jwd.task2.View view = new by.epam.jwd.task2.View();
+        View view = new View();
         s = s.replaceAll("\\s", "");
         double a = logic.calculator(s);
         view.outPut(a);
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        by.epam.jwd.task2.Calculator calculator = new by.epam.jwd.task2.Calculator();
+        Calculator calculator = new Calculator();
         calculator.calc("(2 + 2) * 2");
         calculator.calc("2 + 2 * 4 - (2 + 3 * 2) / 2 + (-10)");
         calculator.calc("2 + 2 * 2");
